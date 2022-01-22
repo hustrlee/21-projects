f9336d95f10400f8ea90b8d068a782d0094a4b78
Ryan Li <hustrlee@gmail.com>
2022-01-22 12:12:59 +0800

    -

--- 1-mnist/download.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-mnist_train, info = tfds.load(
-    name="mnist", split="train", data_dir="./mnist_data", with_info=True)
-assert isinstance(mnist_train, tf.data.Dataset)
-print(mnist_train)
-
-fig = tfds.show_examples(info, mnist_train)
